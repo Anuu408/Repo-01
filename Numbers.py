6015014c8938e0119d153854f3748d70d492255b
Anuu408 <klsvppcs23anushka011@gmail.com>
2025-02-27 15:58:19 +0530

    Create Numbers.py

---
+++ Numbers.py
@@ -0,0 +1,15 @@
+#INPUT    
+a=int(input("Enter the First number:"))
+b=int(input("Enter the Second number:"))
+
+#OPERATIONS
+Add=a+b
+Sub=a-b
+Mulp=a*b
+Div=a/b
+
+#DISPLAY 
+print("Addition=",Add)
+print("Subtraction=",Sub)
+print("Multiplication=",Mulp)
+print("Division=",Div)
