fb6ed5645dc874af9cabd890d5a4f28d18c3d491
Anuu408 <klsvppcs23anushka011@gmail.com>
2025-02-27 16:04:01 +0530

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -6,10 +6,11 @@ b=int(input("Enter the Second number:"))
 Add=a+b
 Sub=a-b
 Mulp=a*b
-Div=a/b
+Mod=a%b
 
 #DISPLAY 
+print("Mathmetical operations!")
 print("Addition=",Add)
 print("Subtraction=",Sub)
 print("Multiplication=",Mulp)
-print("Division=",Div)
+print("Reminder=",Mod)
